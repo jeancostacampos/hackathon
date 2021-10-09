7e0dcc22c885cac206bfd69dc1fee97e8cd0c5df
Lucas Franzolin <lucasfranzolin@gmail.com>
2021-10-09 16:14:08 -0300

    before aws test

---
+++ src/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+
+const PORT = 5000
+const { TELEGRAM_API_TOKEN, NGROK_SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}`
+const URI = `/webhook/${TELEGRAM_API_TOKEN}`
+const WEBHOOK_URL =
+  process.env.NODE_ENV === 'production' ? URI : NGROK_SERVER_URL + URI
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const init = async () => {
+  console.log('Initializing Webhook')
+  const { data } = await axios.get(
+    `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
+  )
+  if (data.ok) console.log('OK')
+  else console.log('Error')
+}
+
+const respondMsg = async (chat_id, text) =>
+  await axios.post(`${TELEGRAM_API}/sendMessage`, { chat_id, text })
+
+app.post(URI, async (req, res) => {
+  const chatId = req.body.message.chat.id
+  const text = req.body.message.text
+
+  console.log(
+    `${req.body.message.chat.first_name} ${req.body.message.chat.last_name} diz:`,
+    text
+  )
+
+  await respondMsg(chatId, text)
+
+  return res.send()
+})
+
+app.listen(PORT, async () => {
+  console.log('Server is running on port', PORT)
+  await init()
+})
